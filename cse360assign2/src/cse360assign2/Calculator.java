1815b4fd56be9b824d29fd075fe47571b1d9d504
kjngu <kjngu@KyleLaptop>
2019-02-25 13:06:05 -0700

    Initial commit of project.

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,86 @@
+
+package cse360assign2;
+/*
+ * Author: Kyle Nguyen
+ * ClassID: 507
+ * Assignment#: 2
+ * Description: This is an application mimics the
+ * functionality of a calculator
+ */
+
+/*
+ * Calculator represents the total value
+ * after executed computations.
+ */
+
+
+public class Calculator 
+{
+
+	private int total;
+	
+	/*
+	 * Class constructor
+	 */
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * Returns the class variable total
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/*
+	 * Adds a new value to the current total
+	 * 
+	 * @param  value  the value to be added to the current total
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/*
+	 * Subtracts the new value from the current total
+	 * 
+	 * @param  value  the value to be subtracted from the current total
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/*
+	 * Multiplies the new value to the current total
+	 * 
+	 * @param  value  the value to be multiplied by the current total
+	 */
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	/*
+	 * Divides the current total by the new value.  If the new 
+	 * value is zero, the current total becomes zero.
+	 * 
+	 * @param  value  the value the total is divided by
+	 */
+	public void divide (int value) 
+	{
+		
+	}
+	
+	/*
+	 * Prints the history of computations done by the user.
+	 */
+	public String getHistory () 
+	{
+		return "";
+	}
+}
